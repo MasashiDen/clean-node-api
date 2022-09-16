7b9fc948111ec234217cee7d15d0ae24e8f417ce
MasashiDen <victor.ofuji@gmail.com>
2022-09-16 07:05:50 -0300

    add jest

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage', // gerar no diretorio coverage nossa cobertura, pra saber quais arquivos estao cobertos de testes
+  coverageProvider: 'v8'
+}
